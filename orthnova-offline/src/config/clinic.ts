a9c21bd296d3eeb092e89e3a092315010c137719
Ritikesh88 <ritikesh.mahapatra@gmail.com>
2025-12-17 13:36:40 +0530

    Delete orthnova-offline directory

--- orthnova-offline/src/config/clinic.ts
+++
@@ -1,6 +0,0 @@
-export const CLINIC_NAME = 'ORTHONOVA POLYCLINIC';
-export const CLINIC_REG_NO = 'Regd. No: XXXXX';
-export const CLINIC_ADDRESS_LINE_1 = '123, Main Street, City';
-export const CLINIC_ADDRESS_LINE_2 = 'State, Country - 000000';
-export const CLINIC_CONTACT = 'Phone: +91-90000 00000 | Email: info@orthonova.example';
-export const CLINIC_FOOTER = 'Thank you for choosing OrthoNova Polyclinic';
